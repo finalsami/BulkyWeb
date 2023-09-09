9bc5ece5eec598fc0b9ec1adaa2da35c6e10dacc
finalsami <finalsami@gmail.com>
2023-09-09 23:56:35 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, DisplayOrder = 1, Name = "Action", IsActive = true },
+                new Category { Id = 2, DisplayOrder = 1, Name = "SciFi", IsActive = true },
+                new Category { Id = 3, DisplayOrder = 1, Name = "History", IsActive = true },
+                new Category { Id = 4, DisplayOrder = 1, Name = "War", IsActive = true });
+        }
+     
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
