83047a6f951809b043d2021db9f8ad7d8e7c8f00
finalsami <finalsami@gmail.com>
2023-09-10 13:05:06 +0530

    Cimitted

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Controllers
 {
@@ -23,14 +24,91 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display order can not be execlty match with same Name");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "test is an invalid value");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created Successfully";
+
                 return RedirectToAction("Index", "Category");
 
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromdb = _db.Categories.Find(id);
+            //alternate
+            //Category? categoryFromdb1 = _db.Categories.FirstOrDefault(u => u.Id==id );
+            //Category? categoryFromdb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (categoryFromdb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromdb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("Name", "Display order can not be execlty match with same Name");
+            //}
+            //if (obj.Name != null && obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("", "test is an invalid value");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+
+                return RedirectToAction("Index", "Category");
+
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromdb = _db.Categories.Find(id);
+
+            if (categoryFromdb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromdb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index", "Category");
+
+        }
     }
 }
+
--- Models/Category.cs
+++ Models/Category.cs
@@ -12,6 +12,7 @@ namespace BulkyWeb.Models
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,10,ErrorMessage ="Please Enter between 1 to 10")]
         public int DisplayOrder { get; set; }
         public bool IsActive { get; set; } = true;
     }
